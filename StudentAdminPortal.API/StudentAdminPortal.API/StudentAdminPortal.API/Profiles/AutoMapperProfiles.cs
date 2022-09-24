a5d218e62e912affa87921a14543682e4ae91c52
aDNpz <74315928+aDNpz@users.noreply.github.com>
2022-09-24 14:18:10 +0200

    Add files via upload

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/StudentAdminPortal.API/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using StudentAdminPortal.API.DomainModels;
+using DataModels = StudentAdminPortal.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, Student>()
+            .ReverseMap();
+
+            CreateMap<DataModels.Gender, Gender>()
+            .ReverseMap();
+
+            CreateMap<DataModels.Address, Address>()
+            .ReverseMap();
+        }
+    }
+}
